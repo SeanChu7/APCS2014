3ae3032b050a6ee2aa4dcc4bc6694bf5eca379cf
cdelano266 <kim.thunderbird@gmail.com>
2014-11-05 13:15:03 -0500

    Update SuperArray.java

--- SuperArray.java
+++ SuperArray.java
@@ -1,15 +1,36 @@
 public class SuperArray{
   protected Object[] data;
   private int size;
+  private int emptyspace;
   public SuperArray(){
     data = new Object[100];
-    size = 200;
+    size = 0;
+    emptyspace = 100;
   }
   public SuperArray(int i){
     data = new Object[i];
-    size = 2*i;
+    size = 0;
+    emptyspace = i;
   }
-  public void add(Object x){
-    
+  public String toString(){
+    String a = "[";
+    for(int i = 0; i < data.length; i++){
+      a += data[i] + " ";
+    }
+    a += "]";
+    return a;
+  }
+  public void resize(){
+    data = new Object[size + 10];
+    emptyspace += 10;
+  }
+  public void add(Object o){
+    if(emptyspace != 0){
+      data[size] = o;
+    }
+    else{
+      resize();
+      data[size] = o;
+    }
   }
 }
