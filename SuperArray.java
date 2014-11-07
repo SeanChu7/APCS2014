c8a92be2349a7aa37521e59058e37de61a937463
cdelano266 <kim.thunderbird@gmail.com>
2014-11-06 19:50:09 -0500

    Update SuperArray.java

--- SuperArray.java
+++ SuperArray.java
@@ -13,7 +13,7 @@ public class SuperArray{
     emptyspace = i;
   }
   public String toString(){
-    String a = "[ ";
+    String a = "[";
     for(int i = 0; i < size; i++){
       a += data[i] + " ";
     }
@@ -22,68 +22,92 @@ public class SuperArray{
   }
   public void resize(){
     Object[] temp = new Object[size];
-    for(int i = 0; i < temp.length; i++){
-      temp[i] = data[i];
-    }
-    data = new Object[size + 10];
-    for(int i = 0; i < temp.length; i++){
-      data[i] = temp[i];
-    }
-    emptyspace += 10;
+	for(int i = 0; i < temp.length; i++){
+		temp[i] = data[i];
+	}
+	data = new Object[size + 10];
+	for(int i = 0; i < temp.length; i++){
+		data[i] = temp[i];
+	}
+	emptyspace += 10;
   }
   public int Size(){
-    return size;
+	return size;
   }
   public Object get(int i){
-    if(get > size - 1){
-      return "null";
-    }
-    else{
-      return data[get];
-    }
+	if(i > size - 1){
+		return "null";
+	}
+	else{
+		return data[i];
+	}
   }
   public Object remove(int i){
-    Object a = get(int i);
-    if(a == "null"){
-      return "null";
-    }
-    else{
-      a = data[i];
-      data[i] = "null";
-      return a;
-    }
+	Object a = get(i);
+	if(a == "null"){
+		return "null";
+	}
+	else{
+		a = data[i];
+		data[i] = "null";
+		return a;
+	}
   }
   public Object set(Object o, int i){
-    Object b = remove(int i);
-    if(b == "null"){
-      return "null";
-    }
-    else{
-      data[i] = o;
-      return b;
-    }
+	Object b = remove(i);
+	if(b == "null"){
+		return "null";
+	}
+	else{
+		data[i] = o;
+		return b;
+	}
   }
-  public void add(Object o, int i){
-    if(index > size){
-      if(emptyspace != 0){
-        data[size] = o;
-        size++;
-        emptyspace --;
-      }
-      else{
-        resize();
-        data[size] = o;
-        size++;
-        emptyspace --;
-      }
-    }
-    else if(index < 0){
-      
-    }
-    else{
-      for(int i = index; i < size; i++){
-        
-      }
-    }
+  public void add(Object o, int p){
+	if(p > size){
+		if(emptyspace != 0){
+		  data[size] = o;
+		}
+		else{
+		  resize();
+		  data[size] = o;
+		}
+		size++;
+		emptyspace--;
+	}
+	else if(p< 0){
+	}
+	else{
+		for(int i = size - 1; i > p; i--){
+			if(emptyspace != 0){
+				data[i + 1] = data[i];
+			}
+			else{
+			  resize();
+			  data[i + 1] = data[i];
+			}
+		}
+		data[p] = o;
+		size++;
+		emptyspace--;
+	}
+  }
+  public static void main(String[] args){
+	SuperArray a = new SuperArray(3);
+	a.add("Yo",500);
+	int i = 0;
+	while(i < 10){
+		a.add(i,500);
+		i++;
+	}
+	Object p = a.toString();
+	System.out.println(p + "\n");
+	p = a.get(500);
+	System.out.println(p + "\n");
+	p = a.get(1);
+	System.out.println(p + "\n");
+	a.remove(5);
+	p = a.toString();
+	System.out.println(p + "\n");
   }
 }
